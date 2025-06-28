299d2ad014d0664f1c665d7e5e63507fc4f941f3
AgustinPalopoli <156222901+AgustinPalopoli@users.noreply.github.com>
2025-06-28 00:26:23 -0300

    Update settings.py

--- notes/settings.py
+++ notes/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sx$s$h#tge-xvdv0u-r4nvlhr&zk86sslk--t^75_0=#q+vmsb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
